e5a34a06eec3700cb5f6e12b24676af95552557c
Zeeshan Khatri <zeeshan.khatri@gmail.com>
2020-06-13 19:29:47 +0500

    Initial Commit

---
+++ ConsoleAppWithEF/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+
+namespace ConsoleAppWithEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ClassDb1Context();
+            foreach(var item in context.Users)
+            {
+                Console.WriteLine($"Id: {item.Id}");
+                Console.WriteLine($"Name: {item.Name}");
+                Console.WriteLine($"Email: {item.Email}");
+                Console.WriteLine($"Password: {item.Password}");
+            }
+
+            var user = new User();
+            Console.WriteLine("Enter Id: ");
+            user.Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Name: ");
+            user.Name = Console.ReadLine();
+
+            Console.WriteLine("Enter Email: ");
+            user.Email = Console.ReadLine();
+
+            Console.WriteLine("Enter Password: ");
+            user.Password = Console.ReadLine();
+
+            context.Add(user);
+            context.SaveChanges();
+        }
+    }
+
+    class ClassDb1Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Class1Db;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+    class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+    }
+
+}
---
+++ ConsoleDBApp/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ConsoleDBApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var connectionString = "Server=(localdb)\\mssqllocaldb;Database=Class1Db;Trusted_Connection=True;MultipleActiveResultSets=true";
+                var connection = new SqlConnection(connectionString);
+                connection.Open();
+
+                Console.WriteLine("Connetion succeed.");
+                var query = "SELECT * FROM Users";
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                var reader = command.ExecuteReader();
+                Console.WriteLine("Reading records.");
+                while (reader.Read()) {
+                    Console.WriteLine($"Id: {reader["Id"]}");
+                    Console.WriteLine($"Name: {reader["Name"]}");
+                    Console.WriteLine($"Email: {reader["Email"]}");
+                    Console.WriteLine($"Password: {reader["Password"]}");
+                }
+                reader.Close();
+
+                Console.WriteLine("Press 'N' for adding new record:");
+                Console.WriteLine("Press 'X' to exit:");
+                var line = Console.ReadLine();
+                while(line != "X")
+                {
+                    if (line == "N")
+                    {
+                        Console.WriteLine("Enter Id: ");
+                        var id = Console.ReadLine();
+
+                        Console.WriteLine("Enter Name: ");
+                        var name = Console.ReadLine();
+
+                        Console.WriteLine("Enter Email: ");
+                        var email = Console.ReadLine();
+
+                        Console.WriteLine("Enter Password: ");
+                        var password = Console.ReadLine();
+
+                        var insertCommandText = $"INSERT INTO Users (Id, Name, Email, Password) VALUES({id}, '{name}', '{email}', '{password}')";
+                        Console.WriteLine(insertCommandText);
+                        var insertCommand = connection.CreateCommand();
+                        insertCommand.CommandText = insertCommandText;
+                        var rows = insertCommand.ExecuteNonQuery();
+                        Console.WriteLine($"Rows effected: {rows}");
+                    }
+
+                    line = Console.ReadLine();
+                }
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
